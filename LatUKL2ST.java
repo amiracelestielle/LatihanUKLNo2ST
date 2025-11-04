ed18703834fa3106f1a5a84c1fc78f3467cf8b53
amiracelestielle <amiracelestielle@gmail.com>
2025-11-04 12:16:26 +0700

    Add files via upload

---
+++ LatUKL2ST.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class LatUKL2ST {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah baris: ");
+        int baris = input.nextInt();
+        System.out.print("Masukkan jumlah kolom: ");
+        int kolom = input.nextInt();
+
+        int[][] matriksA = new int[baris][kolom];
+        int[][] matriksB = new int[baris][kolom];
+        int[][] hasil = new int[baris][kolom];
+
+        System.out.println("\nMasukkan elemen matriks A:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print("A[" + i + "][" + j + "] = ");
+                matriksA[i][j] = input.nextInt();
+            }
+        }
+
+        System.out.println("\nMasukkan elemen matriks B:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print("B[" + i + "][" + j + "] = ");
+                matriksB[i][j] = input.nextInt();
+            }
+        }
+
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                hasil[i][j] = matriksA[i][j] + matriksB[i][j];
+            }
+        }
+
+        System.out.println("\nHasil penjumlahan matriks (A + B):");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print(hasil[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        input.close();
+    }
+}
